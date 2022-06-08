1445a06f173dda65281dc221779c37258f0e8927
MotasemZiad <miziad2000@gmail.com>
2022-06-08 11:22:09 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Instagram Clone',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Instagram'),
+        ),
+        body: const Center(
+          child: Text("Let's start building this app"),
+        ),
+      ),
+    );
+  }
+}
