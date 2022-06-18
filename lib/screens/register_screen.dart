c5a34fda9eabab7128df9b52514d19d43e2fd62b
MotasemZiad <miziad2000@gmail.com>
2022-06-19 00:27:52 +0300

    finish auth UI design

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,31 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:instagram_clone/screens/register_screen.dart';
+import 'package:instagram_clone/screens/login_screen.dart';
 import 'package:instagram_clone/utils/colors.dart';
 import 'package:instagram_clone/widgets/custom_text_field.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({Key? key}) : super(key: key);
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  State<RegisterScreen> createState() => _RegisterScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _RegisterScreenState extends State<RegisterScreen> {
+  late TextEditingController _usernameController;
   late TextEditingController _emailController;
   late TextEditingController _passwordController;
+  late TextEditingController _bioController;
 
   @override
   void initState() {
     super.initState();
+    _usernameController = TextEditingController();
     _emailController = TextEditingController();
     _passwordController = TextEditingController();
+    _bioController = TextEditingController();
   }
 
   @override
   void dispose() {
+    _usernameController.dispose();
     _emailController.dispose();
     _passwordController.dispose();
+    _bioController.dispose();
     super.dispose();
   }
 
@@ -48,6 +54,37 @@ class _LoginScreenState extends State<LoginScreen> {
               const SizedBox(
                 height: 48.0,
               ),
+              Stack(
+                children: [
+                  const CircleAvatar(
+                    radius: 64.0,
+                    backgroundColor: Colors.transparent,
+                    backgroundImage: NetworkImage(
+                        'https://images.unsplash.com/photo-1655374631048-1416d81cd20a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=498&q=80'),
+                  ),
+                  Positioned(
+                    bottom: -10,
+                    left: 80,
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.add_a_photo,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 24.0,
+              ),
+              CustomTextField(
+                controller: _usernameController,
+                hintText: 'Enter your username',
+                keyboardType: TextInputType.name,
+              ),
+              const SizedBox(
+                height: 16.0,
+              ),
               CustomTextField(
                 controller: _emailController,
                 hintText: 'Enter your email',
@@ -63,13 +100,21 @@ class _LoginScreenState extends State<LoginScreen> {
                 isObscured: true,
               ),
               const SizedBox(
+                height: 16.0,
+              ),
+              CustomTextField(
+                controller: _bioController,
+                hintText: 'Enter your biography',
+                keyboardType: TextInputType.text,
+              ),
+              const SizedBox(
                 height: 24.0,
               ),
               InkWell(
                 onTap: () {},
                 child: Container(
                   child: const Text(
-                    'Login',
+                    'Register',
                     style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
                   ),
                   width: 150.0,
@@ -98,21 +143,19 @@ class _LoginScreenState extends State<LoginScreen> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  const Text("Don't have an account?"),
+                  const Text("Already have an account?"),
                   const SizedBox(
                     width: 4.0,
                   ),
                   GestureDetector(
                     onTap: () {
                       Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const RegisterScreen(),
-                        ),
-                      );
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const LoginScreen()));
                     },
                     child: const Text(
-                      "Register",
+                      "Login",
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                         decoration: TextDecoration.underline,
